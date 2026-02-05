b5cdaaefeca69d6ac6f53916b46a770585ca18c8
Alan <musangealain@gmail.com>
2026-02-05 13:24:39 +0200

    Create landing page template with header, faculties, and footer

--- website/views.py
+++ website/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 def home(request):
-    return HttpResponse("Welcome to Univesity of Kigali")
+    return render(request, 'website/home.html')
